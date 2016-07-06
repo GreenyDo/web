10f82dca3aa04738e1cd22f6e9c3c1a0e732eaee
GreenyDo <GreenyDo>
2016-07-06 22:09:02 +0800

    go

--- public/javascripts/web_button.js
+++ public/javascripts/web_button.js
@@ -1,4 +1,5 @@
 $(function(){
+	// 演示
 	$("#web_content .button_code:eq(0)").click(function(){
 		 	window.open('https://github.com/greenydo/module');
 	});
@@ -8,8 +9,19 @@ $(function(){
 	$("#web_content .button_code:eq(2)").click(function(){
 		 	window.open('https://github.com/greenydo/module3');
 	});
-	// 
-	$cssBoxList = $("web_content .button_code:gt(2)");
+	// 布局
+	$("#web_content .button_code:eq(3)").click(function(){
+		 	window.open('/web_code_tpls/layout1.html');
+	});
+	$("#web_content .button_code:eq(4)").click(function(){
+		 	window.open('/web_code_tpls/layout3.html');
+	});
+	$("#web_content .button_code:eq(5)").click(function(){
+		 	window.open('/web_code_tpls/layout4.html');
+	});
+	$("#web_content .button_code:eq(6)").click(function(){
+		 	window.open('/web_code_tpls/layout2.html');
+	});
 	// 
 	$("#codebox1").load("web_code_tpls/css1.html");
 	$("#codebox2").load("web_code_tpls/css2.html");
